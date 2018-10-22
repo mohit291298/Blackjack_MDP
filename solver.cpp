2318b07218c212b943862a1e6b1e9be5cbc05453
mohit291298 <mohitgupta291298@gmail.com>
2018-10-22 18:03:26 +0530

    reward function

--- solver.cpp
+++ solver.cpp
@@ -17,7 +17,8 @@ double P;
 int NUM_STATES;
 double VALUES[NUM_STATES];
 Action OPT_ACTIONS[NUM_STATES];
-
+double P_face,P_non_face;
+double ways_to_sum[22];
 
 //function to convert state to int for indexing into array
 int state_to_int(State s){
@@ -46,6 +47,53 @@ double transition(State initial, Action a, State final){
 
 }
 
+double calc_ways_to_sum(){
+	ways_to_sum[0] = 0;
+	//A
+	ways_to_sum[1] = P_non_face;
+	//AA and 2
+	ways_to_sum[2] = ways_to_sum[1]*P_non_face + P_non_face;
+
+	for(int i = 3;i<=21;i++){
+		// 3 to 9 sum cases
+		if(i<=9){
+			for(int j=i-1;j>0;j--){
+				// getting j valued sum and then getting (i-j) valued card(always non_face till i = 9)
+				ways_to_sum[i] += ways_to_sum[j]*P_non_face;
+			}
+			// getting i valued card only(Non face)
+			ways_to_sum[i] += P_non_face;
+		}
+		else if(i==10){
+			for(int j=i-1;j>0;j--){
+				// getting j valued sum and then getting (i-j) valued card(always non_face for i = 10)
+				ways_to_sum[i] += ways_to_sum[j]*P_non_face;
+			}
+			// getting 10 valued card only
+			ways_to_sum[i] += P_face;
+		}
+		else if(i==11){
+			for(int j=i-1;j>(i-10);j--){
+				// getting j valued sum and then getting (i-j) valued card(non_face for j>(i-10))
+				ways_to_sum[i] += ways_to_sum[j]*P_non_face;
+			}
+				// getting i-10 valued sum and then getting 10 valued card(face)
+			ways_to_sum[i] += ways_to_sum[i-10]*P_face;
+			// getting 11 valued card only
+			ways_to_sum[i] += P_non_face;
+		}
+		else{
+			for(int j=i-1;j>(i-10);j--){
+				// getting j valued sum and then getting (i-j) valued card(non_face for j>(i-10))
+				ways_to_sum[i] += ways_to_sum[j]*P_non_face;
+			}		
+				// getting i-10 valued sum and then getting 10 valued card(face)
+			ways_to_sum[i] += ways_to_sum[i-10]*P_face;
+				// getting i-11 valued sum and then getting 11 valued card(non_face)			
+			ways_to_sum[i] += ways_to_sum[i-11]*P_non_face;
+		}
+	}
+}
 //reward function for taking stand or double on a given state
 double reward(State s, Action a){
 	
@@ -66,6 +114,7 @@ double bellmanBackup(State s){
 	}
 	val_split = sum_split*2 - 1;
 	/* calculating V_n(s) for hit */
+
 	double val_hit;
 	double sum = 0.0;
 	for(int state = 0; state < NUM_STATES; state++){
